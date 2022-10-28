a5bfb1d7d93884dd5a5c1f45a5f5e7f64d149fe2
Rafael de Luca <deluca1712@gmail.com>
2022-10-28 10:30:37 -0300

    Criando a consulta e executando no CommandLineRunner

--- src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,44 @@
 package com.devsuperior.uri2602;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.uri2602.projections.CustomerNameProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
+
 @SpringBootApplication
-public class Uri2602Application {
+public class Uri2602Application implements CommandLineRunner {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// O que for colocado no método run, vai ser executado no início da aplicação
+		
+		List<CustomerNameProjection> list = customerRepository.searchName("RS");
+		List<CustomerNameProjection> list2 = customerRepository.searchName("MG");
+		
+		System.out.println("\nClientes do Rio Grande do Sul");
+		
+		for (CustomerNameProjection object : list) {
+			System.out.println("Nome:" + object.getName());
+		}
+		
+		System.out.println("\nClientes de Minas Gerais");
+		
+		for (CustomerNameProjection object : list2) {
+			System.out.println("Nome:" + object.getName());
+		}
+		
+		
 	}
 }
---
+++ src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -0,0 +1,22 @@
+package com.devsuperior.uri2602.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.uri2602.entities.Customer;
+import com.devsuperior.uri2602.projections.CustomerNameProjection;
+
+@Repository
+public interface CustomerRepository extends JpaRepository <Customer, Long> {
+	
+	//consulta customizada para retornar apenas os atributos desejados
+	//consulta nativa com sql raiz
+	@Query(nativeQuery = true, value = "SELECT name "
+			+ "FROM customers "
+			+ "WHERE state = :state")
+	List<CustomerNameProjection> searchName(String state); 
+
+}
